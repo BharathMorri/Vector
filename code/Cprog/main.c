e89ace9601f6516e9a0f774a3b8c8bb4921cc887
BharathMorri <127733657+BharathMorri@users.noreply.github.com>
2023-07-07 10:54:33 +0530

    Add files via upload

--- code/Cprog/main.c
+++ code/Cprog/main.c
@@ -18,12 +18,12 @@ int main()
 
  // Creating a 2x1 matrix q2 and assigning values 
  q2 = createMat(2,1);
- q2[0][0] = a*a/k;
+ q2[0][0] = pow(a,2)/k;
  q2[1][0] = k;
 
  // Matrix multiplication of q1 and q2, storing the result in q
  q = matmul(q1,q2,2,2,1);
- r = matmul(transpose(e2,2,1),q,2,2,1);
+ r = matmul(transpose(e2,2,1),q,1,2,1);
  c = 1/(2*(1-(a*cosine)/k))*r[0][0];
  A = np_array(c*cosine,c*sine);
  // Saving matrix A to file
